89126cf2c1e0a1b97aa10cd3f98526470618cfee
Yaaanaaa <57559670+Yaaanaaa@users.noreply.github.com>
2020-04-18 16:36:22 +0300

    Add files via upload

---
+++ Test 2/2/test2.js
@@ -0,0 +1,4 @@
+var a = 2;
+var b = 20;
+var c = 5;
+console.log((b*b)-4*a*c);
---
+++ Test 2/3/test3.js
@@ -0,0 +1,10 @@
+var z = 10;
+if (z>10){
+	console.log('z>10');
+}
+else if (z<10) {
+	console.log('z<10');
+}
+else if (z==10) {
+	console.log('z=10');
+}
